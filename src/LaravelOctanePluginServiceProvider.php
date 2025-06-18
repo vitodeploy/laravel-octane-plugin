4d773601f2e83494d6e3e72f1adc32e1a123dd20
Saeed Vaziry <mr.saeedvaziry@gmail.com>
2025-06-18 19:48:59 +0200

    fix composer

--- src/LaravelOctanePluginServiceProvider.php
+++ src/LaravelOctanePluginServiceProvider.php
@@ -17,28 +17,30 @@ class LaravelOctanePluginServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
-        RegisterSiteFeature::make('laravel', 'laravel-octane')
-            ->label('Laravel Octane')
-            ->description('Enable Laravel Octane for this site')
-            ->register();
-        RegisterSiteFeatureAction::make('laravel', 'laravel-octane', 'enable')
-            ->label('Enable')
-            ->form(DynamicForm::make([
-                DynamicField::make('alert')
-                    ->alert()
-                    ->label('Alert')
-                    ->description('Make sure you have already set the `OCTANE_SERVER` in your `.env` file'),
-                DynamicField::make('port')
-                    ->text()
-                    ->label('Octane Port')
-                    ->default(8000)
-                    ->description('The port on which Laravel Octane will run.'),
-            ]))
-            ->handler(Enable::class)
-            ->register();
-        RegisterSiteFeatureAction::make('laravel', 'laravel-octane', 'disable')
-            ->label('Disable')
-            ->handler(Disable::class)
-            ->register();
+        $this->app->booted(function () {
+            RegisterSiteFeature::make('laravel', 'laravel-octane')
+                ->label('Laravel Octane')
+                ->description('Enable Laravel Octane for this site')
+                ->register();
+            RegisterSiteFeatureAction::make('laravel', 'laravel-octane', 'enable')
+                ->label('Enable')
+                ->form(DynamicForm::make([
+                    DynamicField::make('alert')
+                        ->alert()
+                        ->label('Alert')
+                        ->description('Make sure you have already set the `OCTANE_SERVER` in your `.env` file'),
+                    DynamicField::make('port')
+                        ->text()
+                        ->label('Octane Port')
+                        ->default(8000)
+                        ->description('The port on which Laravel Octane will run.'),
+                ]))
+                ->handler(Enable::class)
+                ->register();
+            RegisterSiteFeatureAction::make('laravel', 'laravel-octane', 'disable')
+                ->label('Disable')
+                ->handler(Disable::class)
+                ->register();
+        });
     }
 }
